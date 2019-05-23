fa9476b528622367912bcd3acd9a1c10a2df0931
Flavio Miranda <flavioms7@hotmail.com>
2019-05-23 11:44:20 -0300

    First Commit

---
+++ src/main/java/com/app/ws/projetologinjwt/ProjetoLoginJwtApplication.java
@@ -0,0 +1,13 @@
+package com.app.ws.projetologinjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLoginJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLoginJwtApplication.class, args);
+	}
+
+}
